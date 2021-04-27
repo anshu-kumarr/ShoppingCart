f567fde6e335c29e22de29cb23e006e5dc058c56
anshu-loconav <anshu.kumar@loconav.com>
2021-04-27 18:01:43 +0530

    started cart implementation

--- src/Cart.js
+++ src/Cart.js
@@ -1,13 +1,41 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import '@fortawesome/fontawesome-svg-core'
+import React, { useState } from 'react'
+import FontAwesome from 'react-fontawesome'
+import styled from 'styled-components'
 
 function Cart() {
+
+  const [{ count }, setCart] = useState({ cartItems: [], count: 0, total: 0 });
   return (
-    <div>
-      <FontAwesomeIcon icon="coffee" />
-    </div>
+    <CartIconDisplay>
+      <FontAwesome
+        name="shopping-cart"
+        size="3x"
+      />
+      <CartItemCount>
+        <CartCountDisplay>{count}</CartCountDisplay>
+      </CartItemCount>
+    </CartIconDisplay>
   )
 }
 
 export default Cart
+
+const CartIconDisplay = styled.div`
+padding:10px;
+background-color:black;
+color:white;
+`
+const CartItemCount = styled.div``
+
+const CartCountDisplay = styled.span`
+    position: absolute;
+    bottom: 12px;
+    right: 10px;
+    display: inline-block;
+    padding: 2px 4px .2px;
+    background-color: #e6bd08;
+    border-radius: 10px;
+    font-weight: 600;
+    font-size: .9rem;
+    color:black;
+`
